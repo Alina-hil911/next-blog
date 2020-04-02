52a1f0856ef6b2aea9aa701cab9bbef5d64f8acd
Lefty <asasabahali@gmail.com>
2020-04-02 19:15:05 +0300

    changed app to functional component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,29 @@
-import App, { AppContext } from "next/app";
+import { AppContext } from "next/app";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
-import { Store } from "redux";
-
 import { makeStore } from "../redux/store";
 import GlobalStyle from "../GlobalStyle";
+
 import Layout from "../components/Layout/Layout";
-interface Props {
-  store: Store;
-}
 
-class MyApp extends App<Props> {
-  static async getInitialProps({ Component, ctx }: AppContext) {
-    return {
-      pageProps: Component.getInitialProps
-        ? await Component.getInitialProps(ctx)
-        : {},
-    };
-  }
+const customApp = props => {
+  const { Component, pageProps, store } = props;
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <GlobalStyle></GlobalStyle>
+    </Provider>
+  );
+};
 
-  render() {
-    const { Component, pageProps, store } = this.props;
+customApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
+  return {
+    pageProps: Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {},
+  };
+};
 
-    return (
-      <Provider store={store}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        <GlobalStyle></GlobalStyle>
-      </Provider>
-    );
-  }
-}
-
-export default withRedux(makeStore)(MyApp);
+export default withRedux(makeStore)(customApp);
